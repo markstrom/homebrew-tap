62fc4b171abeadd0b8d0b872df2c9e0e75d2296a
Anders Markström <1905972+markstrom@users.noreply.github.com>
2026-09-25 23:48:42 +0200

    Sorla 1.2.0

--- Casks/sorla.rb
+++ Casks/sorla.rb
@@ -1,6 +1,6 @@
 cask "sorla" do
-  version "1.1.0"
-  sha256 "af595279764a012954af11d43a5e1a13b86a1b3a799b4a0649641c80fe323f4f"
+  version "1.2.0"
+  sha256 "40cbfed899a013291a5acb751be90d3563666d8c46faf4d59a08cb01df51e225"
 
   url "https://github.com/markstrom/sorla/releases/download/v#{version}/Sorla-#{version}.dmg"
   name "Sorla"
